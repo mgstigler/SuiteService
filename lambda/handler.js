dec55c39d9686f1a634458e335b4599744fc020b
Maddie Stigler <mstigler@captechventures.com>
2017-07-25 21:47:05 -0400

    Laura's changes

--- lambda/handler.js
+++ lambda/handler.js
@@ -24,55 +24,72 @@ module.exports.SuiteService = (event, context, callback) => {
 let handlers = {
     //Handles the launch request
     'LaunchRequest': function () {
-        this.emit(':ask', 'Welcome to Hotel Service!', 'Try saying food service.');
+        this.emit(':ask', 'Welcome to Suite Service, ' + guestInformation.FName + '!', 'Try saying food service.');
     },
-    'OrderTowelIntent': function () {
-        let message = "Please send towels to Room " + guestInformation.RoomNumber;
+    'RequestSingularServiceIntent': function () {
+        let service = this.event.request.intent.slots.requestedSingularService.value;
+        let message = "Please send " + service + " to Laura.";
         let topic = "arn:aws:sns:us-east-1:202274289241:TowelService";
-        // alertService.sendAlert(message, topic, null);
-        this.emit(':ask', 'Of course. We will send a set of towels to your room right away ' + JSON.stringify(guestInformation.FName) + '. Do you need anything else?', 'Try saying I would like order something else.');
+        // towelService_1.towelService.sendAlert(message, topic, null);
+        this.emit(':tell', 'Of course. We will send ' + service + ' to your room right away');
     },
-    'FoodServiceIntent': function () {
-        let food = this.event.request.intent.slots.foodItem.value;
-        foodService_1.foodService.getFoodInformation(food, foodInfo => {
-            console.info("Food Info: " + JSON.stringify(foodInfo.Index));
-            let message = "Please send " + food + " to Room " + guestInformation.RoomNumber;
-            let topic = "arn:aws:sns:us-east-1:202274289241:TowelService";
-            // alertService.sendAlert(message, topic, null);
-            foodService_1.foodService.updateRating(foodInfo);
-            var imageObj = {
-                smallImageUrl: bucketPath + JSON.stringify(foodInfo.Index) + '.jpg',
-                largeImageUrl: bucketPath + JSON.stringify(foodInfo.Index) + '.jpg'
-            };
-            cardTitle = JSON.stringify(foodInfo.FoodItem);
-            cardContent = "Rating: " + JSON.stringify(foodInfo.Rating);
-            this.emit(':askWithCard', 'We are sending ' + food + ' your way, ' + guestInformation.FName, cardTitle, cardContent, imageObj);
-        });
-    },
-    'MenuIntent': function () {
-        foodService_1.foodService.getMenu(menu => {
-            cardTitle = menu.speech + ' Menu';
-            cardContent = menu.items.join(", and ");
-            this.emit(':askWithCard', 'We are serving ' + menu.speech + ' now.  This includes ' + menu.items.join(", and ") + '. What can I get for you?', cardTitle, cardContent);
-        });
-    },
-    'AMAZON.StopIntent': function () {
-        // State Automatically Saved with :tell
-        this.emit(':tell', `Goodbye.`);
-    },
-    'AMAZON.CancelIntent': function () {
-        // State Automatically Saved with :tell
-        this.emit(':tell', `Goodbye.`);
-    },
-    'SessionEndedRequest': function () {
-        // Force State Save When User Times Out
-        this.emit(':saveState', true);
-    },
-    'AMAZON.HelpIntent': function () {
-        this.emit(':ask', `What would you like to do?`, `What would you like to do?`);
-    },
-    'Unhandled': function () {
-        this.emit(':ask', `What would you like to do?`, `What would you like to do?`);
+    'RequestedPluralServiceIntent': function () {
+        let number = this.event.request.intent.slots.requestNumber.value;
+        let service = this.event.request.intent.slots.requestedPluralService.value;
+        let message = "Please send " + number + service + " to Laura.";
+        let topic = "arn:aws:sns:us-east-1:202274289241:TowelService";
+        // towelService_1.towelService.sendAlert(message, topic, null);
+        this.emit(':tell', 'Of course. We will send ' + number + service + ' to your room right away');
     }
 };
+'FoodServiceIntent';
+function () {
+    let food = this.event.request.intent.slots.foodItem.value;
+    foodService_1.foodService.getFoodInformation(food, foodInfo => {
+        console.info("Food Info: " + JSON.stringify(foodInfo.Index));
+        let message = "Please send " + food + " to Room " + guestInformation.RoomNumber;
+        let topic = "arn:aws:sns:us-east-1:202274289241:TowelService";
+        // alertService.sendAlert(message, topic, null);
+        foodService_1.foodService.updateRating(foodInfo);
+        var imageObj = {
+            smallImageUrl: bucketPath + JSON.stringify(foodInfo.Index) + '.jpg',
+            largeImageUrl: bucketPath + JSON.stringify(foodInfo.Index) + '.jpg'
+        };
+        cardTitle = JSON.stringify(foodInfo.FoodItem);
+        cardContent = "Rating: " + JSON.stringify(foodInfo.Rating);
+        this.emit(':askWithCard', 'We are sending ' + food + ' your way, ' + guestInformation.FName, cardTitle, cardContent, imageObj);
+    });
+}
+'MenuIntent';
+function () {
+    foodService_1.foodService.getMenu(menu => {
+        cardTitle = menu.speech + ' Menu';
+        cardContent = menu.items.join(", and ");
+        this.emit(':askWithCard', 'We are serving ' + menu.speech + ' now.  This includes ' + menu.items.join(", and ") + '. What can I get for you?', cardTitle, cardContent);
+    });
+}
+'AMAZON.StopIntent';
+function () {
+    // State Automatically Saved with :tell
+    this.emit(':tell', `Goodbye.`);
+}
+'AMAZON.CancelIntent';
+function () {
+    // State Automatically Saved with :tell
+    this.emit(':tell', `Goodbye.`);
+}
+'SessionEndedRequest';
+function () {
+    // Force State Save When User Times Out
+    this.emit(':saveState', true);
+}
+'AMAZON.HelpIntent';
+function () {
+    this.emit(':ask', `What would you like to do?`, `What would you like to do?`);
+}
+'Unhandled';
+function () {
+    this.emit(':ask', `What would you like to do?`, `What would you like to do?`);
+}
+;
 //# sourceMappingURL=handler.js.map
--- lambda/handler.ts
+++ lambda/handler.ts
@@ -27,15 +27,24 @@ let handlers = {
 
   //Handles the launch request
   'LaunchRequest': function () {
-    this.emit(':ask', 'Welcome to Hotel Service!', 'Try saying food service.');
+    this.emit(':ask', 'Welcome to Suite Service, ' + guestInformation.FName + '!', 'Try saying food service.');
   },
 
-  'OrderTowelIntent': function () {
-    let message = "Please send towels to Room " + guestInformation.RoomNumber;
-    let topic = "arn:aws:sns:us-east-1:202274289241:TowelService";   
-    // alertService.sendAlert(message, topic, null);
-    this.emit(':ask', 'Of course. We will send a set of towels to your room right away ' + JSON.stringify(guestInformation.FName) + '. Do you need anything else?', 'Try saying I would like order something else.');
+  'RequestSingularServiceIntent': function () {
+    let service = this.event.request.intent.slots.requestedSingularService.value;
+    let message = "Please send " + service + " to Laura.";
+    let topic = "arn:aws:sns:us-east-1:202274289241:TowelService";
+    // towelService_1.towelService.sendAlert(message, topic, null);
+    this.emit(':tell', 'Of course. We will send ' + service + ' to your room right away');
   },
+  'RequestedPluralServiceIntent': function () {
+    let number = this.event.request.intent.slots.requestNumber.value;
+    let service = this.event.request.intent.slots.requestedPluralService.value;
+    let message = "Please send " + number + service + " to Laura.";
+    let topic = "arn:aws:sns:us-east-1:202274289241:TowelService";
+    // towelService_1.towelService.sendAlert(message, topic, null);
+    this.emit(':tell', 'Of course. We will send ' + number + service + ' to your room right away');
+  ),
 
   'FoodServiceIntent': function() {
       let food = this.event.request.intent.slots.foodItem.value;
