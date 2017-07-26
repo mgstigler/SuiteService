995c36df55ae7cb1ea0d6de2091a19b599b5c390
Maddie Stigler <mstigler@captechventures.com>
2017-07-26 17:07:26 -0400

    added GET for alerts

--- Alerts/UpdateAlerts/handler.ts
+++ Alerts/UpdateAlerts/handler.ts
@@ -1,43 +1,46 @@
 'use strict';
-Object.defineProperty(exports, "__esModule", { value: true });
-const AWS = require("aws-sdk");
-const alertService_1 = require("../lambda/Services/alertService");
+import * as AWS from "aws-sdk";
+import {alertService} from '../../lambda/Services/alertService'
+
 module.exports.UpdateAlert = (event, context, callback) => {
     console.info("Received event: ", JSON.stringify(event, null, 2));
-    let docClient = new AWS.DynamoDB.DocumentClient();
+
+    let docClient = new AWS.DynamoDB.DocumentClient()
+
     // Update the item, unconditionally,
-    alertService_1.alertService.alertGuest(event.Message, event.PhoneNumber, null);
+    alertService.alertGuest(event.Message, event.PhoneNumber, null);
+
     let params = {
-        TableName: "Alerts",
-        Key: {
+        TableName:"Alerts",
+        Key:{
             "RoomNumber": event.RoomNumber
         },
         UpdateExpression: "set FName = :f, LName = :l, PhoneNumber = :p, isActive = :a",
-        ExpressionAttributeValues: {
+        ExpressionAttributeValues:{
             ":f": event.FName,
             ":l": event.LName,
             ":p": event.PhoneNumber,
             ":a": 0
         },
-        ReturnValues: "UPDATED_NEW"
+        ReturnValues:"UPDATED_NEW"
     };
+
     let response = {
         "statusCode": 200,
-        "message": ''
+        "message":''
     };
-    docClient.update(params, function (err, data) {
+
+    docClient.update(params, function(err, data) {
         if (err) {
             response.statusCode = 500;
             console.error("Unable to update item. Error JSON:", JSON.stringify(err, null, 2));
             response.message = "Unable to update alert";
             callback(null, response);
-        }
-        else {
+        } else {
             console.log("UpdateItem succeeded:", JSON.stringify(data, null, 2));
             response.statusCode = 200;
             response.message = "Updated alert successfully.";
             callback(null, response);
         }
     });
-};
-//# sourceMappingURL=handler.js.map
+}
