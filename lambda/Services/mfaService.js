adee0fdac75b4fb44315e46798d1fc097e8fbbb6
Maddie Stigler <mstigler@captechventures.com>
2017-08-17 16:34:42 -0400

    mfa alert with code

--- lambda/Services/mfaService.js
+++ lambda/Services/mfaService.js
@@ -1,23 +1,32 @@
 'use strict';
-Object.defineProperty(exports, "__esModule", { value: true });
-const AWS = require("aws-sdk");
-let sns = new AWS.SNS();
-let verificationCode = null;
-class MFAService {
-    sendVerificationCode(number, callback) {
+exports.__esModule = true;
+var AWS = require("aws-sdk");
+var sns = new AWS.SNS();
+var verificationCode = null;
+var MFAService = (function () {
+    function MFAService() {
+    }
+    MFAService.prototype.sendVerificationCode = function (number, callback) {
         verificationCode = Math.floor(1000 + Math.random() * 9000);
         console.log(verificationCode);
-        let message = "Your verification code is " + verificationCode;
-        let params = {
+        var message = "Your verification code is " + verificationCode;
+        var params = {
             Message: message,
             PhoneNumber: number
         };
-        let request = sns.publish(params);
-        console.info("This is our request:" + JSON.stringify(request));
+        console.info("request " + JSON.stringify(params));
+        var request = sns.publish(params);
         request.send(callback);
         return;
-    }
-}
+    };
+    MFAService.prototype.verifyCode = function (number, callback) {
+        var verified = false;
+        if (number == verificationCode) {
+            verified = true;
+        }
+        callback(verified);
+    };
+    return MFAService;
+}());
 exports.MFAService = MFAService;
 exports.mfaService = new MFAService();
-//# sourceMappingURL=mfaService.js.map
--- lambda/Services/mfaService.ts
+++ lambda/Services/mfaService.ts
@@ -6,7 +6,7 @@ let verificationCode = null;
 
 export class MFAService {
 
-    sendVerificationCode(number: String, callback) {
+    sendVerificationCode(number: string, callback) {
         verificationCode = Math.floor(1000 + Math.random() * 9000);
         console.log(verificationCode);
         let message = "Your verification code is " + verificationCode;
@@ -14,11 +14,18 @@ export class MFAService {
             Message: message,
             PhoneNumber: number
         };
-
+        console.info("request " + JSON.stringify(params));
         let request = sns.publish(params);
-        console.info("This is our request:" + JSON.stringify(request));
         request.send(callback);
         return;
+    }
+
+    verifyCode(number: number, callback) {
+        let verified = false;
+        if (number == verificationCode) {
+            verified = true;
+        }
+        callback(verified);
     }
 
     
